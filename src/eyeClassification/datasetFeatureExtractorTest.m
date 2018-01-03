4b6ad56ee095bdb352e0afc1388c7c07a41e5961
JordiFructos <jordifructos@gmail.com>
2018-01-03 13:29:12 +0100

    Testing del HOG

--- src/eyeClassification/datasetFeatureExtractorTest.m
+++ src/eyeClassification/datasetFeatureExtractorTest.m
@@ -5,11 +5,11 @@ clear;
 %% Feature extraction
 
 % Loads the images and initialize var
-load('..\..\data\datasetImages.mat');
-vectorTest = [8 4 12; 10 4 12; 12 4 12; 14 4 12; 16 4 12; 18 4 12; 20 4 12; 22 4 12; 24 4 12; %9
-                16 2 12; 16 3 12; 16 4 12; 16 5 12; 16 6 12; %5
-                16 4 9; 16 4 10; 16 4 11; 16 4 12; 16 4 13; 16 4 14; 16 4 15]; %7
-resultTest = zeros(21,4,6); %Fila: cell / black / numBins / acc / eyes / noEyes
+load('..\..\data\datasetImagesTest2.mat');
+vectorTest = [8 4 9; 12 4 9; 16 4 9; 20 4 9; 24 4 9;
+                16 2 9; 16 3 9; 16 4 9; 16 5 9; 16 6 9;
+                16 4 9; 16 4 10; 16 4 11; 16 4 12; 16 4 13];
+resultTest = zeros(15,2,6); %Fila: cell / black / numBins / acc / eyes / noEyes
 
 % Creates the training and testing classes vector
 trainingClasses = [repmat('E',1,numTrainingEyesImages), repmat('N',1,numTrainingNoEyesImages)]';
@@ -21,10 +21,10 @@ testingClasses = [repmat('E',1,numTestingEyesImages), repmat('N',1,numTestingNoE
 
 %% Testing feature extraction
 
-parfor i=1:size(vectorTest)
+parfor i=1:15
     varTest = vectorTest(i,:);
     featureExtractor = featureExtractor();
-    for j=1:4
+    for j=1:2
         % Creates the training and testing feature matrix
         trainingFeatures = featureExtractor.extractFeaturesTesting(trainingDataset,varTest(1),varTest(2),varTest(3));
         testingFeatures = featureExtractor.extractFeaturesTesting(testingDataset,varTest(1),varTest(2),varTest(3));
@@ -51,4 +51,4 @@ end
 
 %% Saves the results of the tests
 
-save('../../data/testingResults.mat', 'resultTest');
+save('../../data/testingResults2.mat', 'resultTest');
