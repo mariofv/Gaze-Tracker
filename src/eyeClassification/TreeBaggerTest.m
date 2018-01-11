d96f5d250e50f71b60b10ff6fdbddcde96878b4a
JordiFructos <jordifructos@gmail.com>
2018-01-11 13:08:40 +0100

    correciones menores

--- src/eyeClassification/TreeBaggerTest.m
+++ src/eyeClassification/TreeBaggerTest.m
@@ -7,13 +7,12 @@ clear;
 % Loads the images and initialize var
 load('..\..\data\datasetFeaturesTest2.mat');
 vectorTest = [10 20 30 40 50 60 70 80 90 100 110 120 130 140 150];
-
-load('..\..\data\testingResults4.mat');
+resultTest = zeros(15,5,4); %Fila: size / acc / eyes / noEyes
 
 
 %% Testing treebagger
 
-parfor i=1:6
+parfor i=1:15
     varTest = vectorTest(i);
     for j=1:5
         % Creates the model 
